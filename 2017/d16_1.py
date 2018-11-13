bb4b170c5521e5c0c8900620a3e50e615f67e2ee
rosiss <noite.ros@gmail.com>
2018-11-12 21:00:58 -0600

    ChalComp: Day 16 part 1 solution finished
    Day 16 part 1 works now!
    I was not taking into account that swapping letters by
    its name will affect the original position of them (duh!).

--- 2017/d16_1.py
+++ 2017/d16_1.py
@@ -1,15 +1,16 @@
+"""--- Day 16: Permutation Promenade ---"""
 
 def get_final_array(arr, ops):
     off = 0
     a = ord('a')
     
     for op in ops:
-        print map(chr, arr)
-        print op
-        print off
         if op[0] == 'p':
             c1, c2 = map(ord, op[1:].split("/"))
-            arr[c1 - a], arr[c2 - a] = arr[c2 - a], arr[c1 - a]
+            
+            p1, p2 = [p for p, c in enumerate(arr) if c1 == c or c2 == c]
+            arr[p1], arr[p2] = arr[p2], arr[p1]
+
             continue
 
         if op[0] == 'x':
@@ -45,8 +46,7 @@ def main():
     ops = raw_input()
 
     arr = range(ord('a'), ord('p') + 1)
-    #arr = ["a", "b", "c", "d", "e"]
-    #arr = map(ord, arr)
+    #arr = map(ord, "padheomkgjfnblic")
     ops = ops.split(",")
 
     print "Order: {}".format("".join(map(chr,arr)))
