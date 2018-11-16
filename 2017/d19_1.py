8e9c0e2086a4494c8af25ee1ce9ded474d3911e6
rosiss <noite.ros@gmail.com>
2018-11-15 23:48:14 -0600

    Chal: Partial implementation of Day 19 part 1 challenge
    Partial implementation of Day 19 part 1 challenge

---
+++ 2017/d19_1.py
@@ -0,0 +1,23 @@
+
+import sys
+
+
+
+
+def traverse_path(grid):
+    current_p = grid[0].index('|')
+
+    while(True):
+        
+
+
+def main():
+    
+    grid = []
+    for line in sys.stdin:
+        grid.append(list(line)[:-1])        
+ 
+
+if __name__ == "__main__":
+    main()
+
